22017b5ac33cd844ba65baef018242974fe70e0d
MuhamedAbdo <mohamedabd09999933@gmail.com>
2025-08-13 19:08:48 +0300

    feat: fix path_provider error and enable Supabase init with kIsWeb check

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,27 @@
 
 import 'package:flutter/material.dart';
 import 'package:smart_sheet/screens/splash_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+const String supabaseUrl = 'https://edytjabmzjtidmtukvxt.supabase.co';
+const String supabaseAnonKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkeXRqYWJtemp0aWRtdHVrdnh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxMDAyMjAsImV4cCI6MjA3MDY3NjIyMH0.xUmC4xHSP5c3kFK-jg7qZCDhrFw8rBGhZkbNdCk7kKw'; // ← عدلها
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // تهيئة Hive
+  await Hive.initFlutter();
+  await Hive.openBox('settings');
+  await Hive.openBox('measurements');
+
+  // ✅ تهيئة Supabase
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
+  );
+
   runApp(const SmartSheetApp());
 }
 
@@ -18,7 +37,7 @@ class SmartSheetApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         useMaterial3: true,
-        fontFamily: 'Cairo', // هنضيف الخط لاحقًا
+        fontFamily: 'Cairo',
       ),
       home: const SplashScreen(),
     );
